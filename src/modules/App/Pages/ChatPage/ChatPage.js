30e94b531a17c2f6bab48ff764057c1a2d2884b0
iamaltamas <altamasalam00@gmail.com>
2023-06-08 02:15:41 +0530

    resuable Input

--- src/modules/App/Pages/ChatPage/ChatPage.js
+++ src/modules/App/Pages/ChatPage/ChatPage.js
@@ -1,13 +1,19 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import TextInput from '../../../Components/InputComponent/InputComponent'
 const ChatPage = () => {
+  const [inputValue,setInputValue] = useState()
+  
+  const textHandle = (event) => {
+    setInputValue(event)
+  }
+  
   return (
     <div className='.container-fluid'>
       <div class="row justify-content-md-center">
 
         <div class="col-md-2">Left</div>
 
-        <div class="col-md-8">Right</div>
+        <div class="col-md-8"><TextInput title="input" onChange={(event)=> textHandle(event)}/></div>
 
       </div>
     </div>
