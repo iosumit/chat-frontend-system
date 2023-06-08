e12fc052ff9887c4314670370c8d649c85c699ad
Sumit Kumar <sk4390789@gmail.com>
2023-06-08 22:22:21 +0530

    Structure Correction

--- src/modules/App/Pages/ChatPage/ChatPage.js
+++ src/modules/App/Pages/ChatPage/ChatPage.js
@@ -1,19 +1,14 @@
-import React,{useState} from 'react'
-import TextInput from '../../../Components/InputComponent/InputComponent'
+import React from 'react'
+import { ChatPdp } from './component/ChatPdp'
+
 const ChatPage = () => {
-  const [inputValue,setInputValue] = useState()
-  
-  const textHandle = (event) => {
-    setInputValue(event)
-  }
-  
   return (
     <div className='.container-fluid'>
       <div class="row justify-content-md-center">
 
         <div class="col-md-2">Left</div>
 
-        <div class="col-md-8"><TextInput title="input" onChange={(event)=> textHandle(event)}/></div>
+        <div class="col-md-8"><ChatPdp /> </div>
 
       </div>
     </div>
---
+++ src/modules/App/Pages/ChatPage/component/ChatPdp.js
@@ -0,0 +1,5 @@
+export function ChatPdp() {
+    return (<>
+        Chat Pdp
+    </>)
+}
