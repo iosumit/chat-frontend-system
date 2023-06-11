08de04a3eb2f9f99ad279e27ac11e5e42cb2ab59
Sumit Kumar <sk4390789@gmail.com>
2023-06-11 16:55:27 +0530

    Appbar Structure

--- src/modules/App/Pages/ChatPage/ChatPage.js
+++ src/modules/App/Pages/ChatPage/ChatPage.js
@@ -4,6 +4,16 @@ import "./ChatPage.css"
 const ChatPage = () => {
   return (
     <div className='container-fluid chatBody'>
+      {/* AppBar Component */}
+      <div className='row'>
+        <div className='col-12 col-md-12 col-lg-4'>
+          Icons<br />Search
+        </div>
+        <div className='col-12 col-md-12 col-lg-8'>
+          Rest Options
+        </div>
+      </div>
+      {/* AppBar Component */}
       <div className='row'>
 
         <div class="col-12 col-md-12 col-lg-4 " style={{ backgroundColor: "gray" }}>Left</div>
