c379f40ef23cf1f97d09322002c69ccfa391116d
Sumit Kumar <sk4390789@gmail.com>
2023-06-11 16:46:34 +0530

    Margin on side Bug Fixed

--- src/modules/App/Pages/ChatPage/ChatPage.js
+++ src/modules/App/Pages/ChatPage/ChatPage.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import ChatPdp  from './component/ChatPdp'
+import ChatPdp from './component/ChatPdp'
 import "./ChatPage.css"
 const ChatPage = () => {
   return (
-    <div className='container chatBody'>
+    <div className='container-fluid chatBody'>
       <div className='row'>
 
-        <div class="col-12 col-md-12 col-lg-4 " style={{backgroundColor:"gray"}}>Left</div>
+        <div class="col-12 col-md-12 col-lg-4 " style={{ backgroundColor: "gray" }}>Left</div>
 
         <div class="col-12 col-md-12 col-lg-8"><ChatPdp /> </div>
       </div>
